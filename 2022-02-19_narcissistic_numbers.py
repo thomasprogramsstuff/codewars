79e5742ebc6d81bf5838d121d44e90d7bf17e5d1
TCHI <87097016+thomasprogramsstuff@users.noreply.github.com>
2022-02-19 13:46:28 -0500

    Update 2022-02-19_narcissistic_numbers.py

--- 2022-02-19_narcissistic_numbers.py
+++ 2022-02-19_narcissistic_numbers.py
@@ -30,3 +30,7 @@ test.assert_equals(narcissistic(7), True, '7 is narcissistic');
 test.assert_equals(narcissistic(371), True, '371 is narcissistic');
 test.assert_equals(narcissistic(122), False, '122 is not narcissistic')
 test.assert_equals(narcissistic(4887), False, '4887 is not narcissistic')
+
+#### Suggested Best Practice (efficient) Solution:
+#def narcissistic(value):
+#    return value == sum(int(x) ** len(str(value)) for x in str(value))
